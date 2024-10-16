dc0822c790493735ae3ed70dd484ea098eea6dd3
Ngo Tung Anh <126256917+DawnBreaker207@users.noreply.github.com>
2024-10-16 15:27:27 +0700

    update

--- Demo/rxjs.ts
+++ Demo/rxjs.ts
@@ -2,10 +2,17 @@ import { fromEvent, map, Observable, scan, throttleTime } from 'rxjs';
 
 const arr1 = [1, 2, 3, 4, 5];
 const arr2 = [5, 6, 7, 8, 9];
-
-const clickButton = document.getElementById('click');
-let showDetail = document.getElementById('show');
-let count = 0;
+const stringSlug = [
+  'Trong trường hợp này',
+  'Câu chuyện này có lẽ không xảy ra',
+  'Đây có lẽ là trường hợp hiếm hoi',
+  'Mà thứ này thực sự có thể xảy ra',
+  'Tôi đôi lúc ức chế đến tột cùng',
+  'Đời tôi chưa bao giờ gặp điều như này cả',
+];
+// const clickButton = document.getElementById('click');
+// let showDetail = document.getElementById('show');
+// let count = 0;
 
 // showDetail.innerHTML = count.toString();
 
@@ -29,28 +36,77 @@ let count = 0;
 
 // Define Observable
 
-const observable = new Observable((subscribe) => {
-  subscribe.next(1);
-  subscribe.next(2);
-  subscribe.next(3);
-  setTimeout(() => {
-    subscribe.next(4);
-    subscribe.complete();
-  }, 1000);
+// const observable = new Observable((subscribe) => {
+//   subscribe.next(1);
+//   subscribe.next(2);
+//   subscribe.next(3);
+//   setTimeout(() => {
+//     subscribe.next(4);
+//     subscribe.complete();
+//   }, 1000);
+// });
+// console.log('Just before subscribe');
+// observable.subscribe({
+//   //* Observer next()
+//   next(x) {
+//     console.log(`Got value ${x}`);
+//   },
+//   //* Observer error()
+//   error(err) {
+//     console.error(`Something wrong occurred ${err}`);
+//   },
+//   //* Observer complete()
+//   complete() {
+//     console.log(`Done`);
+//   },
+// });
+// console.log('Just after subscribe');
+
+const slugify = (val: string) => {
+  if (!val) return '';
+
+  return (
+    String(val)
+      // Spilt accented characters into their base characters and diacritical marks
+      .normalize('NFKC')
+      // Replace VietNamese Characters
+      // "a" and "ă" variants
+      .replace(
+        /(à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ|À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ)/g,
+        'a'
+      )
+      // "e" variants
+      .replace(/(è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ|È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ)/g, 'e')
+      // "i" variants
+      .replace(/(ì|í|ị|ỉ|ĩ|Ì|Í|Ị|Ỉ|Ĩ)/g, 'i')
+      // "o" variants
+      .replace(
+        /(ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ|Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ)/g,
+        'o'
+      )
+      // "u" variants
+      .replace(/(ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ|Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ)/g, 'u')
+      // "y" variants
+      .replace(/(ỳ|ý|ỵ|ỷ|ỹ|Ý|Ỵ|Ỷ|Ỹ)/g, 'y')
+      // "d" variants
+      .replace(/(đ|Đ)/g, 'd')
+      // Remove all the accents, which happen to be all in \u03xx UNICODE
+      .replace(/[\u0300-\u036f]/g, '')
+      // Trim leading or trailing whitespace
+      .trim()
+      // Convert to lower case
+      .toLowerCase()
+      // Remove non-alphanumeric characters
+      .replace(/[^a-z0-9 -]/g, '')
+      // Replace spaces with hyphens
+      .replace(/\s+/g, '-')
+      // Replace consecutive hyphens
+      .replace(/-+/g, '-')
+  );
+};
+
+stringSlug.map((index) => {
+  const transfer = slugify(index);
+
+  console.log(transfer);
 });
-console.log('Just before subscribe');
-observable.subscribe({
-  //* Observer next()
-  next(x) {
-    console.log(`Got value ${x}`);
-  },
-  //* Observer error()
-  error(err) {
-    console.error(`Something wrong occurred ${err}`);
-  },
-  //* Observer complete()
-  complete() {
-    console.log(`Done`);
-  },
-});
-console.log('Just after subscribe');
